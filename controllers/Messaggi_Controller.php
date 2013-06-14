e1ea5a361b5fe7fb4abc21e9e6680282ba6eb3c8
Alessandro Rossi <alessandro.rossi@neropaco.net>
2013-06-14 18:36:08 +0200

    messaggi2

--- controllers/Messaggi_Controller.php
+++ controllers/Messaggi_Controller.php
@@ -20,6 +20,34 @@ class Messaggi_Controller extends \pff\AController
         echo 'Welcome to pff!';
     }
 
-    public function 
+    public function addMessaggio(){
+        $id_personaggio = $_POST['personaggio'];
+        $personaggio = $this->_em->find('/pff/models/personaggio',$id_personaggio);
+        $id_gilda = $_POST['gilda'];
+        $id_posto = $_POST['posto'];
+        if ($id_gilda == 0){
+            $gilda = null;
+        } else{
+            $gilda = $this->_em->find('/pff/models/gilda',$id_gilda);
+        }
+        if ($id_posto == 0){
+            $posto = null;
+        } else {
+            $posto = $this->_em->find('/pff/models/posto',$id_posto);
+        }
+        $messaggio = $_POST['messaggio'];
+        $data = new DateTime();
+
+        $m = new \pff\models\Messaggi();
+        $m->setData($data);
+        $m->setPersonaggio($personaggio);
+        $m->setGilda($gilda);
+        $m->setPosto($posto);
+        $m->setMessaggio($messaggio);
+
+        $this->_em->persist($m);
+        $this->_em->flush();
+        
+    }
 
 }
