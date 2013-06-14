910c9c832dbacd625a30fd6d7c6dafa8ec9e6495
Alessandro Rossi <alessandro.rossi@neropaco.net>
2013-06-14 17:16:54 +0200

    messaggi

--- controllers/Ajax_Controller.php
+++ controllers/Ajax_Controller.php
@@ -19,6 +19,7 @@ class Ajax_Controller extends \pff\AController
     public function messaggi(){
         $id_posto = $this->getParam(0);
         $posto = $this->_em->find('pff/models/posto',$id_posto);
-        $messaggi = $this
+        $messaggi = $this->_em->getRepository('pff/models/messaggi')->findBy(array('posto'=>$posto),array('data'=>'desc'),50);
+        \Doctrine\Common\Util\Debug::dump($messaggi);
     }
 }
---
+++ controllers/Messaggi_Controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: alessandro 
+ * Date: 14/06/13
+ * Time: 17.10
+ * 
+ * @author aleros78<at>gmail.com
+ */
+class Messaggi_Controller extends \pff\AController
+{
+
+    /**
+     * Index action.
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        echo 'Welcome to pff!';
+    }
+
+    public function 
+
+}
